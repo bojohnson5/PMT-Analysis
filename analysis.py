f1dfb23199dda9a34a73c7de5c15cd606c481551
bojohnson5 <bo.johnson5@icloud.com>
2021-06-01 14:09:47 -0400

    start adding fancy fit

--- analysis.py
+++ analysis.py
@@ -7,8 +7,7 @@ import awkward as ak
 import uproot
 
 from scipy.optimize import curve_fit
-from fit_funcs import gaussian
-
+from fit_funcs import deap_expo, deap_gamma, deap_ped, spe
 
 class Rooter:
     """
@@ -65,7 +64,7 @@ class Rooter:
         plt.title('Run ' + self.fi[0:2] + ' Max. Amps.')
         plt.show()
 
-    def view_spectrum(self, wind, n_bins=150, view_wind=(-200, 4000)):
+    def view_spectrum(self, wind, n_bins=150, view_wind=(-200, 4000), y_log=False):
         """
         Histogram the integrated waveforms
 
@@ -80,13 +79,31 @@ class Rooter:
         """
         spec = np.apply_along_axis(np.sum, 1, self.w[:, wind[0]:wind[1] + 1])
         plt.hist(spec[(spec > view_wind[0]) * (spec < view_wind[1])],
-                 bins=n_bins, histtype='step', label='Int. wind. ' + str(wind[0])
-                                                     + '-' + str(wind[1]))
+                 bins=n_bins, histtype='step', label='Int. wind. ' + str(wind[0] * 4)
+                                                     + '-' + str(wind[1] * 4))
         plt.xlabel('Integrated ADC', loc='right')
         plt.ylabel('Count', loc='top')
         plt.title('Run ' + self.fi[0:2] + ' Spectrum')
         plt.legend()
+        if y_log:
+            plt.yscale('log')
         plt.show()
+
+    def gain(self, wind):
+        """
+        Calculate the gain of the PMT as an average of all waveform integrated currents
+        
+        Parameters
+        ----------
+            wind : tuple of ints
+                Integration window stop and start for current integration
+        """
+        spec = np.apply_along_axis(np.sum, 1, self.w[:, wind[0]:wind[1] + 1])
+        volts = np.linspace(0, 2, 4096)
+        spec = spec.astype(int)
+        spec = spec[(spec > 0) * (spec < 4096)]
+        charge = volts[spec] / 50 * 4e-9 / 1.6e-19
+        print(f'{np.average(charge):.2e}')
 
     def view_multi_spec(self, winds, n_bins=150, view_wind=(-200, 4000)):
         """
@@ -104,15 +121,76 @@ class Rooter:
         for wind in winds:
             spec = np.apply_along_axis(np.sum, 1, self.w[:, wind[0]:wind[1] + 1])
             plt.hist(spec[(spec > view_wind[0]) * (spec < view_wind[1])],
-                     bins=n_bins, histtype='step', label='Int. wind. ' + str(wind[0])
-                                                         + '-' + str(wind[1]))
+                     bins=n_bins, histtype='step', label='Int. wind. ' + str(wind[0] * 4)
+                                                         + '-' + str(wind[1] * 4))
         plt.xlabel('Integrated ADC', loc='right')
         plt.ylabel('Count', loc='top')
         plt.title('Run ' + self.fi[0:2] + ' Spectrum')
         plt.legend()
         plt.show()
 
-    def fit_spectrum(self, wind, funcs, bounds, p0s, n_bins=150, view_wind=(-200, 4000)):
+    def fit_spectrum(self, wind, funcs, bounds, p0s, n_bins=150, view_wind=(-200, 4000),
+                     y_log=False, print_res=False):
+        """
+        Fit a spectrum with a given function(s) over a specified range(s)
+
+        Parameters
+        ----------
+            wind : tuple of ints
+                Where to stop and start the integration window, as indicies not times
+            funcs : list of functions
+                Functions that will be fitted over specified range
+            bounds : list of tuples
+                Range where the associated function should be fitted to
+            p0s: list of lists
+                Initial paramter guesses for fits
+            n_bins : int, optional
+                Number of histogram bins
+            view_wind : tuple of ints, optional
+                Viewing window for histogram
+        """
+        spec = np.apply_along_axis(np.sum, 1, self.w[:, wind[0]:wind[1] + 1])
+        spec = spec[(spec > view_wind[0]) * (spec < view_wind[1])]
+        hist, bin_b = np.histogram(spec, bins=n_bins)
+        bin_w = np.diff(bin_b)
+        bin_c = bin_b[:-1] + bin_w / 2
+        plt.hist(spec[(spec > view_wind[0]) * (spec < view_wind[1])],
+                 bins=n_bins, histtype='step', label='Int. wind. ' + str(wind[0] * 4)
+                 + '-' + str(wind[1] * 4))
+
+        fit_num = 1
+        params = []
+        for func, bound, p0 in zip(funcs, bounds, p0s):
+            x_fit = bin_c[(bin_c > bound[0]) * (bin_c < bound[1])]
+            y_fit = hist[(bin_c > bound[0]) * (bin_c < bound[1])]
+            popt, _ = curve_fit(func, x_fit, y_fit, p0=p0)
+            params.append(popt)
+            x = np.linspace(view_wind[0], view_wind[1], 1000)
+            y = func(x, *popt)
+            plt.plot(x[y > 1e-2], y[y > 1e-2], '--', label='fit ' + str(fit_num))
+            fit_num += 1
+        if print_res:
+            adj_mean = params[1][1] - params[0][1]
+            res = adj_mean / params[1][2]
+            max_i = np.argmax(hist)
+            peak_offset = 5
+            peak_i = np.argmax(hist[max_i + peak_offset:])
+            min_i = np.argmin(hist[max_i:peak_i])
+            pv = hist[max_i + peak_offset:][peak_i] / hist[max_i:peak_i][min_i]
+            pv_3 = hist[max_i + peak_offset:][peak_i] / \
+                hist[self._find_nearest(bin_c, adj_mean * 0.3)]
+            plt.text(1000, 1500, f'P/V: {pv:.2f}\nP/V (0.3PE): {pv_3:.2f}\nRes: '
+                                 f'{res:.2f}\nSPE Peak: {adj_mean:.2f}')
+        if y_log:
+            plt.yscale('log')
+        plt.legend()
+        plt.xlabel('Integrated ADC', loc='right')
+        plt.ylabel('Count', loc='top')
+        plt.title('Run ' + self.fi[0:2] + ' Spectrum and Fits')
+        plt.show()
+
+    def deap_fit_spectrum(self, wind, funcs, bounds, p0s, n_bins=150, view_wind=(-200, 4000), 
+                          y_log=False):
         """
         Fit a spectrum with a given function(s) over a specified range(s)
 
@@ -149,21 +227,11 @@ class Rooter:
             y = func(x, *popt)
             plt.plot(x, y, '--', label='fit ' + str(fit_num))
             fit_num += 1
-        adj_mean = params[1][1] - params[0][1]
-        res = adj_mean / params[1][2]
-        max_i = np.argmax(hist)
-        peak_offset = 5
-        peak_i = np.argmax(hist[max_i + peak_offset:])
-        min_i = np.argmin(hist[max_i:peak_i])
-        pv = hist[max_i + peak_offset:][peak_i] / hist[max_i:peak_i][min_i]
-        pv_3 = hist[max_i + peak_offset:][peak_i] / \
-            hist[self._find_nearest(bin_c, adj_mean * 0.3)]
-        plt.legend()
-        plt.text(1000, 1500, f'P/V: {pv:.2f}\nP/V (0.3PE): {pv_3:.2f}\nRes: '
-                             f'{res:.2f}\nSPE Peak: {adj_mean:.2f}')
         plt.xlabel('Integrated ADC', loc='right')
         plt.ylabel('Count', loc='top')
         plt.title('Run ' + self.fi[0:2] + ' Spectrum and Fits')
+        if y_log:
+            plt.yscale('log')
         plt.show()
 
     def _find_nearest(self, array, value):
@@ -191,6 +259,7 @@ class Rooter:
         pre = 0
         late = 0
         after = 0
+        i = 0
         for waveform in self.w:
             x = np.arange(0, len(waveform))
             w = waveform - spe_thre
@@ -204,9 +273,12 @@ class Rooter:
                 after_s = spe_x + after_wind[0]
                 after_e = spe_x + after_wind[1]
                 spe += 1
-                pre += self._count_pulses(waveform[pre_s:pre_e], pulse_thre)
-                late += self._count_pulses(waveform[late_s:late_e], pulse_thre)
-                after += self._count_pulses(waveform[after_s:after_e], pulse_thre)
+                new_pre = self._count_pulses(waveform[pre_s:pre_e], pulse_thre)
+                new_late = self._count_pulses(waveform[late_s:late_e], pulse_thre)
+                new_after = self._count_pulses(waveform[after_s:after_e], pulse_thre)
+                pre += new_pre
+                late += new_late
+                after += new_after
         print(f'Pre-Pulsing: {pre / spe * 100 : .2f}%')
         print(f'Late-Pulsing: {late / spe * 100 : .2f}%')
         print(f'After-Pulsing: {after / spe * 100 : .2f}%')
@@ -226,7 +298,7 @@ class Rooter:
         x = np.arange(0, len(waveform) * 4, 4)
         w = waveform - thre
         cross = x[1:][w[1:] * w[:-1] < 0]
-        count += len(cross) / 2
+        count += len(cross) // 2
 
         return count
 
@@ -234,8 +306,17 @@ class Rooter:
 if __name__ == '__main__':
     r = Rooter('12.root')
     r.max_amplitudes()
-    r.view_waveform(3)
-    r.view_multi_spec([(160, 170), (150, 180), (162, 175)], n_bins=100)
-    r.fit_spectrum((160, 170), [gaussian, gaussian],
-                   [(-200, 200), (500, 900)], [[2000, 0, 50], [200, 500, 100]])
-    r.pre_post_pulsing(200, 0.3 * 200, (2, 23), (6, 37), (37, 6250))
+    #  r.view_waveform(i)
+    #  r.view_spectrum((160, 170), y_log=True)
+    #  r.deap_fit_spectrum((160, 170), [spe_fit], [(-200, 4000)],
+                   #  [[2000, 0, 50, 200, 50, 50, 100, 10, 10, 10, 1]], y_log=True)
+    #  r.view_multi_spec([(160, 170), (150, 180), (162, 175)], n_bins=100)
+    #  r.fit_spectrum((160, 170), [deap_ped, deap_gamma],
+                   #  [(-200, 200), (500, 1000)],
+                   #  [[2000, 0, 50], [400, 10, 5]],
+                   #  y_log=True)
+    #  r.fit_spectrum((160, 170), [gaussian, gaussian],
+                   #  [(-200, 200), (500, 900)], [[2000, 0, 50], [200, 500, 100]])
+    spe_adc = 187.5
+    r.pre_post_pulsing(spe_adc, 0.3 * spe_adc, (2, 23), (6, 37), (37, 6250))
+    #  r.gain((160, 170))
--- fit_funcs.py
+++ fit_funcs.py
@@ -2,7 +2,62 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from scipy.special import gamma
 
-def gaussian(x, p0, p1, p2):
-    return p0 * np.exp(-0.5 * ((x - p1) / p2)**2)
+def deap_expo(x, p0, p1):
+    """
+    Exponential function from Eq. (5) of 'In-situ characterization of the
+    Hamamatsu R5912-HQE photomultiplier tubes used in the DEAP-3600 experiment'
+
+    Parameters
+    ----------
+        p0 : float
+            Amplitude
+        p1 : float
+            Parameter l from Eq. (5)
+    """
+    return p0 * p1 * np.exp(-x * p1)
+
+def deap_ped(x, p0, p1, p2):
+    """
+    Pedastal function from Eq. (6) of 'In-situ characterization of the
+    Hamamatsu R5912-HQE photomultiplier tubes used in the DEAP-3600 experiment'
+
+    Parameters
+    ----------
+        p0 : float
+            Amplitude
+        p1 : float
+            Parameter mu_ped from Eq. (6)
+        p2 : float
+            Parameter sigma_ped from Eq. (6)
+    """
+    return p0 / (np.sqrt(2 * np.pi) * p2) * np.exp(-(x - p1)**2 / (2 * p2**2))
+
+
+def deap_gamma(x, p0, p1, p2):
+    """
+    Gamma function from Eq. (4) of 'In-situ characterization of the
+    Hamamatsu R5912-HQE photomultiplier tubes used in the DEAP-3600 experiment'
+
+    Parameters
+    ----------
+        p0: float
+            Amplitude
+        p1 : float
+            Parameter mu from Eq. (4)
+        p2 : float
+            Parameter b from Eq. (4)
+    """
+    return p0 / (p1 * p2 * gamma(1 / p2)) * \
+        np.power(x / (p1 * p2), 1 / p2 - 1) * np.exp(-x / (p1 * p2))
+
+def spe(x, p0, p1, p2, p3, p4, p5, p6, p7):
+    """
+    SPE fit corresponding to Eq. (5) of the paper
+    """
+    main_gamma = deap_gamma(x, p0, p1, p2)
+    sec_gamma = deap_gamma(x, p4, p1 * p4, p2 * p5)
+    expo = deap_expo(x, p6, p7)
+    return main_gamma + sec_gamma + expo * (x <= p4)
 
